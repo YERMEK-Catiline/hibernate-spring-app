0e8a573b0a82bcd895657ca121b518492954734b
CATILINA <ermektoregozhin@gmail.com>
2026-08-11 13:11:13 +0500

    Add PersonDAO skeleton

---
+++ src/main/java/com/toregozhin/springcourse/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package com.toregozhin.springcourse.dao;
+
+import com.toregozhin.springcourse.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    public List<Person> index() {
+        return null;
+    }
+
+    public void show(int id) {
+
+    }
+
+    public void save(Person person) {
+
+    }
+
+    public void update(int id, Person person) {
+
+    }
+
+    public void delete(int id) {
+
+    }
+}
